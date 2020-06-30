796c33a31c817b4b0a90b8094317f13197f65cde
Ezike,Christopher <christopherezikeu2@gmail.com>
2020-06-30 01:27:42 +0100

    initial commit

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const request = require("request");
+const config = require("config");
 const router = express.Router();
 const auth = require("../../Middleware/auth");
 const { check, validationResult } = require("express-validator");
@@ -286,6 +288,32 @@ router.delete("/education/:exp_id", auth, async (req, res) => {
   } catch (error) {
     console.error(error.message);
     restart.status(500).send("Server Error");
+  }
+});
+
+// @route GET api/profile/github/:username
+//@desc   get user repos from github
+//@access Public
+router.get("/github/:username", (req, res) => {
+  try {
+    const options = {
+      url:
+        'https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc&client_id=${config.get("githubClientId")}&client_secret=${config.get("githubSecret")}',
+      method: "GET",
+      headers: { "user-agent": "node.js" },
+    };
+
+    request(options, (error, response, body) => {
+      if (error) console.error(error);
+
+      if (response.statusCode !== 200) {
+        res.status(404).json({ msg: " No Github profile found" });
+      }
+      res.json(JSON.parse(body));
+    });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server Error");
   }
 });
 
