24201d18270ad1f8eafb261268af11abc6aea84b
Ezike,Christopher <christopherezikeu2@gmail.com>
2020-07-02 19:08:29 +0100

    initial commit

---
+++ models/Post.js
@@ -0,0 +1,53 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "users",
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  avatar: {
+    type: String,
+  },
+  likes: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: "users",
+      },
+    },
+  ],
+  comments: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: "users",
+      },
+      text: {
+        type: String,
+        required: true,
+      },
+      avatar: {
+        type: String,
+      },
+      date: {
+        type: Date,
+        dafault: Date.now,
+      },
+    },
+  ],
+  date: {
+    type: Date,
+    deafult: Date.now,
+  },
+});
+
+module.exports = Post = mongoose.model("post", PostSchema);
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -298,7 +298,7 @@ router.get("/github/:username", (req, res) => {
   try {
     const options = {
       url:
-        'https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc&client_id=${config.get("githubClientId")}&client_secret=${config.get("githubSecret")}',
+        'http://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc&client_id=${config.get("githubClientId")}&client_secret=${config.get("githubSecret")}',
       method: "GET",
       headers: { "user-agent": "node.js" },
     };
@@ -307,12 +307,12 @@ router.get("/github/:username", (req, res) => {
       if (error) console.error(error);
 
       if (response.statusCode !== 200) {
-        res.status(404).json({ msg: " No Github profile found" });
+        return res.status(404).json({ msg: " No Github profile found" });
       }
       res.json(JSON.parse(body));
     });
   } catch (error) {
-    console.error(error.message);
+    console.error(err.message);
     res.status(500).send("Server Error");
   }
 });
